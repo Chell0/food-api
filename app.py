bb025d8e123b0648f0f775341b643e5b1f75ec7e
Chell0 <machelgabriel@gmail.com>
2018-09-08 03:56:07 +0300

    update file structure

--- app.py
+++ app.py
@@ -1,55 +1,49 @@
 from flask import Flask, jsonify
 from flask import abort
-from flask import make_response
 from flask import request
 from flask import url_for
 
-
 app = Flask(__name__)
-
-
 
 """
 Orders stored in data structures
 """
 orders = [
-	{
-		'id': 1,
-		'title': 'Chicken Burger',
-		'description': 'Lemon basted chicken patty, Mayo shredded lettuce, Tomato slice, Sesame seed bun',
-		'done': False
-	},
-	{
-		'id': 2,
-		'title': 'Chicken Salad',
-		'description': 'Chicken breast strips, Cucumber, Lettuce, Carrots, Tomato wedges, 1000 island dressing',
-		'done': False
-	},
-	{
-		'id': 3,
-		'title': 'Veggie Burger',
-		'description': 'Veggie patty, Shredded lettuce, Tomato slice, Dills, Relish sauce, Sesame seed bun',
-		'done': False
-	},
-	{
-		'id': 4,
-		'title': 'Green Salad',
-		'description': 'Cucumber, Lettuce, Red onion, Tomato wedges, Green pepper',
-		'done': False
-	}
+    {
+        'id': 1,
+        'title': 'Chicken Burger',
+        'description': 'Lemon basted chicken patty, Mayo shredded lettuce, Tomato slice, Sesame seed bun',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Chicken Salad',
+        'description': 'Chicken breast strips, Cucumber, Lettuce, Carrots, Tomato wedges, 1000 island dressing',
+        'done': False
+    },
+    {
+        'id': 3,
+        'title': 'Veggie Burger',
+        'description': 'Veggie patty, Shredded lettuce, Tomato slice, Dills, Relish sauce, Sesame seed bun',
+        'done': False
+    },
+    {
+        'id': 4,
+        'title': 'Green Salad',
+        'description': 'Cucumber, Lettuce, Red onion, Tomato wedges, Green pepper',
+        'done': False
+    }
 ]
-
 
 
 def make_public_order(order):
     new_order = {}
     for food in order:
         if food == 'id':
-            new_order['uri'] = url_for('get_order', order_id = order['id'], _external = True)
+            new_order['uri'] = url_for('get_order', order_id=order['id'], _external=True)
         else:
             new_order[food] = order[food]
     return new_order
-
 
 
 """REST Endpoints for Resources (CRUD)"""
@@ -57,25 +51,31 @@ def make_public_order(order):
 """
 The GET method used to return the data of all the orders
 """
+
+
 @app.route('/food/api/v1/orders', methods=['GET'])
 def get_orders():
-	return jsonify({'orders': [make_public_order(order) for order in orders]})
+    return jsonify({'orders': [make_public_order(order) for order in orders]})
 
 
 """
 The GET method used to return the data of a single order
 """
+
+
 @app.route('/food/api/v1/orders/<int:order_id>', methods=['GET'])
 def get_order(order_id):
     order = filter(lambda o: o['id'] == order_id, orders)
     if len(order) == 0:
         abort(404)
-    return jsonify({'order': make_publice_order(order[0])})
-	
+    return jsonify({'order': make_public_order(order[0])})
+
 
 """
 The POST method, which we will use to place a new order
 """
+
+
 @app.route('/food/api/v1/orders', methods=['POST'])
 def create_order():
     if not request.json or not 'title' in request.json:
@@ -93,6 +93,8 @@ def create_order():
 """
 The PUT method, which we will use to update the status of an order
 """
+
+
 @app.route('/food/api/v1/orders/<int:order_id>', methods=['PUT'])
 def update_order(order_id):
     order = filter(lambda o: o['id'] == order_id, orders)
@@ -115,6 +117,8 @@ def update_order(order_id):
 """
 The DELETE method, which we will use to delete an order
 """
+
+
 @app.route('/food/api/v1/orders/<int:order_id>', methods=['DELETE'])
 def delete_order(order_id):
     order = filter(lambda o: o['id'] == order_id, orders)
@@ -125,5 +129,4 @@ def delete_order(order_id):
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
-	
+    app.run(debug=True)
